e9e6c18c7a34eb01e1391e1c43d05cf1c861cd88
ntur101 <ntur101@aucklanduni.ac.nz>
2025-11-25 15:16:03 +1300

    updated build yml to actual routes

--- thingsboard_scraper.py
+++ thingsboard_scraper.py
@@ -21,6 +21,12 @@ def get_offline_nodes(url, wait_time=5, headless=True):
     chrome_options = Options()
     if headless:
         chrome_options.add_argument('--headless')
+    
+    # Additional options for running in CI/server environments
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--window-size=1920,1080')
 
     # Initialize the driver
     driver = webdriver.Chrome(options=chrome_options)
